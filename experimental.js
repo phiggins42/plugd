523dec7c65c521324c9fcafcc2bcdc2b4028c659
phiggins <phiggins@59045108-9d77-11dd-ada3-7767e702ac26>
2009-01-10 13:18:55 +0000

    adding in theoretical experimental functions. no-op functions for calling or chaining.

--- experimental.js
+++ experimental.js
@@ -3,6 +3,27 @@ dojo.require("dijit.dijit");
 	
 	var d = dojo, dij = dijit;
 	
+	d._nop = function(){
+		// summary: A global no-op function to reused in all no-op situations
+		//
+		// example:
+		// |	my.workAround = dojo.isIE ? function(){ /* janky */ } : dojo._nop;
+		//
+	}
+	
+	d._nopReturn = function(){
+		// summary: A global no-op function, like `dojo.noop`, but for use 
+		//		in `dojo.NodeList` no-op situations where the NodeList needs
+		//		to be returned for forther chaining.
+		//
+		// example:
+		//	Create a safe-NodeList function for an IE-specific issue
+		//	|	dojo.extend(dojo.NodeList, {
+		//	|		janky: dojo.isIE ? function(){ /* workaround */ } : d._nopReturn
+		//	|	});
+		return this; // Anything
+	}
+	
 	d.extend(d.NodeList, {
 		widget: {
 			
