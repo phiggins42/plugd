046c2ff519500ef59cd03713d31a253c2629d04b
phiggins <phiggins@59045108-9d77-11dd-ada3-7767e702ac26>
2009-02-23 14:52:55 +0000

    more docs.

--- script.js
+++ script.js
@@ -8,23 +8,29 @@
 		
 	d.addScript = function(src, callback){
 		// summary: A simplified version of `dojo.io.script.get`. Handle's cross-domain loading of
-		// 		resources that may not otherwise be provided as JSONP and meant entirely to be 
+		//		resources that may not otherwise be provided as JSONP and meant entirely to be 
 		//		consumed upon loading.
 		//
 		// src: String
-		//		The URL of a script to inject into this page
+		//		The URL of a script to inject into this page. Can be local or cross-domain. 
 		//
 		// callback: Function?
 		//		Optional callback function to execute on completion of the script. No failure 
-		//		can be trapped. A timeout could easily be incorporated. 
+		//		can be trapped. A timeout could easily be incorporated. The event which triggered
+		//		the success is passed to the callback. 
 		//
 		// example:
 		//	|		dojo.addScript("http://example.com/js.js", function(e){ /* js.js is ready */ });
 		//
-		var s = d.create("script", { 
-			src: src, type: "text/javascript"
-		}, h);
-		
+		// example:
+		//	|		dojo.addScript("foo.js", dojo.hitch(this, "handler"));
+		//
+		// example: 
+		//	|		dojo.addScript(dojo.moduleUrl("dijit", "Dialog.js"), function(e){
+		//	|			// only Dialog.js onload, not it's require() calls. use `dojo.require` + `dojo.addOnLoad`
+		//	|		});
+		//
+		var s = d.create("script", { src: src, type: "text/javascript" }, h);
 		if(callback){
 			var c = d.connect(s, ev, function(e){
 				// loaded is the non-cached version, complete indicates cached. readystate fires for both. 
