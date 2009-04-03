b949f0c93fc8a38aa76747307e813b828107a540
phiggins <phiggins@59045108-9d77-11dd-ada3-7767e702ac26>
2009-04-03 11:27:47 +0000

    A better trigger() implemenetation that works as a compliment to dojo.connect

--- trigger.js
+++ trigger.js
@@ -1,11 +1,34 @@
 dojo.provide("plugd.trigger");
 (function(d){
+
+	var isfn = d.isFunction,
 	
-	d.trigger = function(node, event){
+		triggerEvent = function(node, event){
+			// summary: Helper for `dojo.trigger`, which handles the DOM cases. We should never
+			// be here without a nodeNode reference and a string eventname.
+			node = d.byId(node); 
+			event = event && event.slice(0, 2) == "on" ? event.slice(2) : event;
+			if(d.doc.createEvent){
+				var evObj = d.doc.createEvent("HTMLEvents");
+				evObj.initEvent(event, true, true);
+				node.dispatchEvent(evObj);
+			}else if(d.doc.createEventObject){
+				node.fireEvent("on" + event);
+			}
+		},
+	
+		triggerObject = function(obj, event){
+			// summary: Helper for `dojo.trigger`, which handles the Object cases.
+			return d.hitch.apply(this, arguments)();
+		}
+	;
+	
+	d.trigger = function(obj, event){
 		// summary: Trigger some event whose origin is `node`
 		//
-		// node: String|DomNode
-		//		An ID, or DomNode reference, from which to trigger the event
+		// obj: String|DomNode|Object|Function
+		//		An ID, or DomNode reference, from which to trigger the event.
+		//		If an Object, fire the `event` in the scope of this object.
 		// 
 		// event: String
 		//		The name of the event to trigger. Can be any DOM level 2 event
@@ -20,18 +43,19 @@ dojo.provide("plugd.trigger");
 		//	|	// or from within dojo.query:
 		//	|	dojo.query("a").onclick(function(){}).trigger("onclick");
 		//
+		// example:
+		//	|	dojo.trigger(obj, "method");
+		//
+		// example:
+		//	|	dojo.trigger(d.global, function(){ /* stuff */ });
+		//
 
-		node = d.byId(node);
-		event = event.slice(0,2) == "on" ? event.slice(2) : event;
-		if(d.doc.createEvent){
-			var evObj = d.doc.createEvent("HTMLEvents");
-			evObj.initEvent(event, true, true);
-			node.dispatchEvent(evObj);
-		}else if(d.doc.createEventObject){
-			node.fireEvent("on" + event);
-		}
+		return (isfn(obj) || isfn(event) || isfn(obj[event])) ? 
+			triggerObject(obj, event) :
+			triggerEvent(obj, event)
+		;
 	};
 	
-	d.NodeList.prototype.trigger = d.NodeList._adaptAsForEach(d.trigger);
+	d.NodeList.prototype.trigger = d.NodeList._adaptAsForEach(triggerEvent);
 	
 })(dojo);
