c50247aa74a22f85f4f2bbc8ac05bc3c7ebbf9fd
phiggins <phiggins@59045108-9d77-11dd-ada3-7767e702ac26>
2009-05-08 07:50:20 +0000

    adding tests ensuring djConfig.conflict works in both cases

---
+++ tests/conflict.js
@@ -0,0 +1,5 @@
+dojo.provide("plugd.tests.base");
+if(dojo.isBrowser){
+	doh.registerUrl("plugd.tests.base", dojo.moduleUrl("plugd.tests", "conflict.html"));
+	doh.registerUrl("plugd.tests.trigger", dojo.moduleUrl("plugd.tests", "conflicttag.html"));
+}
--- tests/module.js
+++ tests/module.js
@@ -2,10 +2,12 @@ dojo.provide("plugd.tests.module");
 
 try{
 	dojo.require("plugd.tests.base");
+	dojo.require("plugd.tests.conflict");
 	dojo.require("plugd.tests.trans");
 	dojo.require("plugd.tests.block");
 	dojo.require("plugd.tests.escape");
 	dojo.require("plugd.tests.magic");
+	dojo.require("plugd.tests.ancestor");
 }catch(e){
 	doh.debug(e);
 }
