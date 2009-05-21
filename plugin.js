867e535dc193734512bac007fa2df413662e472e
phiggins <phiggins@59045108-9d77-11dd-ada3-7767e702ac26>
2009-05-21 12:53:07 +0000

    making the plugin thinger its own module

---
+++ plugin.js
@@ -0,0 +1,51 @@
+dojo.provide("plugd.plugin");
+(function(d){
+	
+	d.plugin = function(pluginNamespace, fn, way){
+		// summary: 
+		//		Define a function to exist on both the `dojo` namespace
+		//		and as a `dojo.query` plugin.
+		//
+		// pluginNamespace: String
+		//		The name of the plugin to use. In development, a warning will
+		//		be thrown if attempting to register a plugin twice. After build,
+		//		(in special cases) no such warning is provided. 
+		//
+		// fn: Function
+		//		The plugin function. Must be defined with (at least) a signature
+		//		of `function(node){}`, node being in the first position. Any further
+		//		arguments are optional.
+		//
+		// way: String?
+		//		Defines which "way" to attached this function `fn` to `dojo.NodeList` 
+		//		(the return of a `dojo.query`). Defaults to "_adaptAsForEach", a typical
+		//		plugin pattern of simply running some function for each node in the list.
+		//
+		// example:
+		//	|	var defaults = { option1: "default" };
+		//	|	dojo.plugin("something", function(node, args){
+		//	|		node = dojo.byId(node);
+		//	|		var options = dojo.mixin({}, defaults, args);
+		//	|		// do something to node	
+		//	|	});
+		//	|
+		//	|	dojo.something("id", {});
+		//	|	// or
+		//	|	dojo.query(".nodes").something({});
+
+
+		//>>excludeStart("safetyFirst", kwArgs.noWarnings == "on");
+		// leave safety in optionally, build with noWarnings=on to disable check
+		if(d[pluginNamespace]){
+			console.warn("cowardly won't clobber '" + pluginNamespace + "' method");
+			return fn; // Function
+		}
+		//>>excludeEnd("safetyFirst");
+	
+		var f = d[pluginNamespace] = fn;
+		d.NodeList.prototype[pluginNamespace] = d.NodeList[way || "_adaptAsForEach"](f);
+	
+		return f; // Function
+	}
+
+})(dojo);
--- tests/base.js
+++ tests/base.js
@@ -2,4 +2,5 @@ dojo.provide("plugd.tests.base");
 if(dojo.isBrowser){
 	doh.registerUrl("plugd.tests.base", dojo.moduleUrl("plugd.tests", "base.html"));
 	doh.registerUrl("plugd.tests.trigger", dojo.moduleUrl("plugd.tests", "trigger.html"));
+	doh.registerUrl("plugd.tests.plugin", dojo.moduleUrl("plugd.tests", "plugin.html"));
 }
