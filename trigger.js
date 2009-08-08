ef217e11ed6c660b45eb9275466dbb013bbb3a60
phiggins <phiggins@59045108-9d77-11dd-ada3-7767e702ac26>
2009-08-08 14:47:56 +0000

    exposing dojo._trigger as fast path, better docs, dropping closed triggerObject

--- trigger.js
+++ trigger.js
@@ -1,38 +1,46 @@
 dojo.provide("plugd.trigger");
 (function(d){
 
-	var isfn = d.isFunction,
-	
-		triggerEvent = function(node, event){
-			// summary: Helper for `dojo.trigger`, which handles the DOM cases. We should never
-			// be here without a nodeNode reference and a string eventname.
-			node = d.byId(node); 
-			event = event && event.slice(0, 2) == "on" ? event.slice(2) : event;
-			if(d.doc.createEvent){
-				var evObj = d.doc.createEvent("HTMLEvents");
-				evObj.initEvent(event, true, true);
-				node.dispatchEvent(evObj);
-			}else if(d.doc.createEventObject){
-				node.fireEvent("on" + event);
-			}
-		},
-	
-		triggerObject = function(){
-			// summary: Helper for `dojo.trigger`, which handles the Object cases.
-			return d.hitch.apply(d, arguments)();
+	d._trigger = function(node, event){
+		// summary: 
+		//		Helper for `dojo.trigger`, which handles the DOM cases. We should never
+		//		be here without a nodeNode reference and a string eventname.
+		node = d.byId(node); 
+		event = event && event.slice(0, 2) == "on" ? event.slice(2) : event;
+		if(d.doc.createEvent){
+			var evObj = d.doc.createEvent("HTMLEvents");
+			evObj.initEvent(event, true, true);
+			node.dispatchEvent(evObj);
+		}else if(d.doc.createEventObject){
+			node.fireEvent("on" + event);
 		}
-	;
+	};
+
+	var isfn = d.isFunction;
 	
 	d.trigger = function(obj, event){
-		// summary: Trigger some event whose origin is `node`
+		// summary: 
+		//		Trigger some event. It can be either a Dom Event, Custom Event, 
+		//		or direct function call. 
+		//
+		// description:
+		//		Trigger some event. It can be either a Dom Event, Custom Event, 
+		//		or direct function call. NOTE: This function does not trigger
+		//		default behavior, only triggers bound event listeneres. eg:
+		//		one cannot trigger("anchorNode", "onclick") and expect the browser
+		//		to follow the href="" attribute naturally.
 		//
 		// obj: String|DomNode|Object|Function
 		//		An ID, or DomNode reference, from which to trigger the event.
-		//		If an Object, fire the `event` in the scope of this object.
-		// 
-		// event: String
+		//		If an Object, fire the `event` in the scope of this object,
+		//		similar to calling dojo.hitch(obj, event)(). The return value
+		//		in this case is returned from `dojo.trigger`
+		//	 
+		// event: String|Function
 		//		The name of the event to trigger. Can be any DOM level 2 event
 		//		and can be in either form: "onclick" or "click" for instance.
+		//		In the object-firing case, this method can be a function or
+		//		a string version of a member function, just like `dojo.hitch`.
 		//
 		// example: 
 		//	|	dojo.connect(node, "onclick", function(e){ /* stuff */ });
@@ -40,22 +48,44 @@ dojo.provide("plugd.trigger");
 		//	|	dojo.trigger(node, "onclick");
 		//
 		// example:
-		//	|	// or from within dojo.query:
+		//	|	// or from within dojo.query: (requires dojo.NodeList)
 		//	|	dojo.query("a").onclick(function(){}).trigger("onclick");
 		//
 		// example:
+		//	|	// fire obj.method() in scope of obj
 		//	|	dojo.trigger(obj, "method");
 		//
 		// example:
+		//	|	// fire an anonymous function:
 		//	|	dojo.trigger(d.global, function(){ /* stuff */ });
 		//
+		// returns: Anything
+		//		Will not return anything in the Dom event case, but will return whatever
+		//		return value is received from the triggered event. 
 
 		return (isfn(obj) || isfn(event) || isfn(obj[event])) ? 
-			triggerObject.apply(d, arguments) : triggerEvent(obj, event);
+			d.hitch.apply(d, arguments)() : d._trigger(obj, event);
 	};
 	
 	// adapt for dojo.query:
-	d.NodeList.prototype.trigger = d.NodeList._adaptAsForEach(triggerEvent);
+	/*=====
+	dojo.extend(dojo.NodeList, {
+		trigger: function(event){
+			// summary:
+			//		Trigger some DOM Event originating from each of the nodes in this
+			//		`dojo.NodeList`. 
+			//
+			// event: String
+			//		Any strig identifier for the event.type to be triggered.
+			//
+			// example:
+			//	|	dojo.query("a").trigger("onclick");
+
+			return this; // dojo.NodeList
+		}
+	});
+	=====*/
+	if(d.NodeList){ d.NodeList.prototype.trigger = d.NodeList._adaptAsForEach(d._trigger); }
 	
 	// if the node.js module is available, extend trigger into that.
 	if(d._Node && !d._Node.prototype.trigger){
@@ -63,16 +93,18 @@ dojo.provide("plugd.trigger");
 			trigger: function(ev){
 				// summary:
 				//		Fire some some event originating from this node.
-				//		Only available if both the trigger and node plugin 
-				//		are enabled. 
+				//		Only available if both the `dojo.trigger` and `dojo.node` plugin 
+				//		are enabled. Allows chaining as all `dojo._Node` methods do.
 				//
 				// ev: String
 				//		Some string event name to fire. eg: "onclick", "submit"
 				//
 				// example:
+				//	|	// fire onlick orginiating from a node with id="someAnchorId"
 				//	|	dojo.node("someAnchorId").trigger("click");
-				triggerEvent(this, ev);
-				return this;
+
+				d._trigger(this, ev);
+				return this; // dojo._Node
 			}
 		});
 	}
