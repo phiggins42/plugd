8bdaf48e6d09d27677f97108afd6470ec2ba06aa
Peter E Higgins <dante@dojotoolkit.org>
2011-01-21 11:09:14 -0500

    whitespace ftl

--- base.js
+++ base.js
@@ -506,21 +506,21 @@ dojo.provide("plugd.base");
 
 	d.throttle = function(cb, wait, thisObj){
 		// summary: 
-		//	Create a function that will only execute once per `wait` periods.
+		//		Create a function that will only execute once per `wait` periods.
 		// description:
-		//	Create a function that will only execute once per `wait` periods
-		//	from last execution when called repeatedly. Useful for preventing excessive
-		//	calculations in rapidly firing events, such as window.resize, node.mousemove
-		//	and so on.
+		//		Create a function that will only execute once per `wait` periods
+		//		from last execution when called repeatedly. Useful for preventing excessive
+		//		calculations in rapidly firing events, such as window.resize, node.mousemove
+		//		and so on.
 		// cb: Function
-		//	The callback to fire. 
+		//		The callback to fire. 
 		// wait: Integer
-		//	time to delay before allowing cb to call again. 
+		//		time to delay before allowing cb to call again. 
 		// thisObj: Object?
-		//	Optional execution context
+		//		Optional execution context
 		var canrun = true; 
 		return function(){
-            if(!canrun) return; 
+			if(!canrun) return; 
 			canrun = false;
 			cb.apply(thisObj || cb, arguments);
 			setTimeout(function(){ 
@@ -531,16 +531,18 @@ dojo.provide("plugd.base");
 
 	d.debounce = function(cb, wait, thisObj){
 		// summary: 
-		//	Create a function that will only execute after `wait` milliseconds
+		//		Create a function that will only execute after `wait` milliseconds
 		// description: 
-		//	Create a function that will only execute after `wait` milliseconds
-		//	of repeated execution. Useful for delaying some event action slightly to allow
-		//	for rapidly-firing events such as window.resize, node.mousemove and so on. 
+		//		Create a function that will only execute after `wait` milliseconds
+		//		of repeated execution. Useful for delaying some event action slightly to allow
+		//		for rapidly-firing events such as window.resize, node.mousemove and so on. 
 		// cb: Function
-		//	A callback to fire. Like hitch() and partial(), arguments passed to the 
-		//	returned function curry along to the original callback. 
+		//		A callback to fire. Like hitch() and partial(), arguments passed to the 
+		//		returned function curry along to the original callback. 
 		// wait: Integer
-		//	Time to spend caching executions before actually executing. 
+		//		Time to spend caching executions before actually executing. 
+		// thisObj: Object?
+		//		Optional execution context. 
 		var timer;
 		return function(){
 			if(timer) clearTimeout(timer);
